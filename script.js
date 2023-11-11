1dda7e51e8e0600932fbd51542206edcc8e8ddfe
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-11 22:17:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const filesInput = document.querySelector('#files-input')
+const downloadCsvButton = document.querySelector('#download-csv-button')
+
+filesInput.addEventListener()
+downloadCsvButton.addEventListener()
