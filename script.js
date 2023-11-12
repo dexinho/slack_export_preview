b1dee41cdafcc85a77ac921a367a9a776d1ba8b9
Jasmin Hidanovic <dexinho008@gmail.com>
2023-11-12 22:26:38 +0100

    added previews

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const filesInput = document.querySelector("#files-input");
 const inputDiv = document.querySelector("#input-div");
 const downloadCsvButton = document.querySelector("#download-csv-button");
 let FINAL_CSV = "";
-let filesCounter = 0;
+let FILES_COUNTER = 0;
+const USERS_DATA = [];
+const USER_ICON_COLORS = {};
 
 filesInput.addEventListener("change", (e) => {
     const files = e.target.files;
-    filesCounter = 0;
+    FILES_COUNTER = 0;
     FINAL_CSV = "Date and Time; Message sender; Message content;\n";
 
     [...files].forEach((file) => {
@@ -14,7 +16,7 @@ filesInput.addEventListener("change", (e) => {
         reader.readAsText(file);
         reader.onload = () => {
             const contents = JSON.parse(reader.result);
-            filesCounter++;
+            FILES_COUNTER++;
 
             for (let i = 0; i < contents.length; i++) {
                 const content = contents[i];
@@ -26,27 +28,38 @@ filesInput.addEventListener("change", (e) => {
                         second: "numeric",
                     }
                 );
-                const senderName = content.user.replace(/[\r\n]/g, '')
-                const textContent = content.text.replace(/[\r\n]/g, '')
+                const senderName = content.user.replace(/[\r\n]/g, "");
+                const messageContent = content.text.replace(/[\r\n]/g, "");
 
-                writeContent({
+                const data = {
                     date,
                     senderName,
-                    textContent,
-                });
+                    messageContent,
+                };
+
+                if (!USER_ICON_COLORS[senderName])
+                    USER_ICON_COLORS[senderName] = `rgb(${Math.floor(
+                        Math.random() * 200 + 50
+                    )}, ${Math.floor(Math.random() * 200 + 50)}, ${Math.floor(
+                        Math.random() * 200 + 50
+                    )})`;
+
+                USERS_DATA.push(data);
+                writeContent(data);
             }
 
-            if (filesCounter === files.length) {
+            if (FILES_COUNTER === files.length) {
                 downloadCsvButton.style.display = "block";
+                previewMessages();
             }
         };
     });
 });
 
-function writeContent({ date, senderName, textContent }) {
+function writeContent({ date, senderName, messageContent }) {
     FINAL_CSV += date + ";";
     FINAL_CSV += senderName + ";";
-    FINAL_CSV += textContent + "\n";
+    FINAL_CSV += messageContent + "\n";
 }
 
 function downloadCSV() {
@@ -58,3 +71,48 @@ function downloadCSV() {
 }
 
 downloadCsvButton.addEventListener("click", downloadCSV);
+const previewDiv = document.querySelector("#preview-div");
+
+const previewMessages = () => {
+    cleanPreviewMessages();
+
+    USERS_DATA.forEach((data) => {
+        const previewSlot = document.createElement("div");
+        const iconDiv = document.createElement("div");
+        const messageDiv = document.createElement("div");
+        const i = document.createElement("i");
+        const userNameDateDiv = document.createElement("div");
+        const userMessage = document.createElement("div");
+        const userNameSpan = document.createElement("span");
+        const dateSpan = document.createElement("span");
+
+        previewSlot.classList.add("preview-slot");
+        iconDiv.classList.add("icon-div");
+        i.classList.add("fa-solid");
+        i.classList.add("fa-user");
+        messageDiv.classList.add("message-div");
+        userNameDateDiv.classList.add("user-name-date-div");
+        userMessage.classList.add("user-message");
+        userNameSpan.classList.add("user-name-span");
+        dateSpan.classList.add("date-span");
+
+        previewDiv.append(previewSlot);
+        previewSlot.append(iconDiv);
+        previewSlot.append(messageDiv);
+        iconDiv.append(i);
+        messageDiv.append(userNameDateDiv);
+        messageDiv.append(userMessage);
+        userNameDateDiv.append(userNameSpan);
+        userNameDateDiv.append(dateSpan);
+
+        i.style.color = USER_ICON_COLORS[data.senderName];
+        userNameSpan.textContent = data.senderName;
+        dateSpan.textContent = data.date;
+        userMessage.textContent = data.messageContent;
+    });
+};
+
+const cleanPreviewMessages = () => {
+    while (previewDiv.childElementCount > 0)
+        previewDiv.removeChild(previewDiv.firstElementChild);
+};
